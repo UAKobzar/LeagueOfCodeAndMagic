8459c1ba02835c7ad592e2871fe2f1331d794ddb
UAKobzar <uakobzar@email.email>
2019-08-09 21:55:13 +0300

    init commit

---
+++ LegendsOfCodeAndMagic/Board.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LegendsOfCodeAndMagic
+{
+    public class Board : ICloneable
+    {
+        public Player[] Players { get; set; }
+
+        public List<Card>[] PlayersBoards { get; set; }
+
+        public Board()
+        {
+            Players = new Player[2];
+            PlayersBoards = new List<Card>[2];
+        }
+
+        public object Clone()
+        {
+            return new Board()
+            {
+                Players = this.Players.Clone() as Player[],
+                PlayersBoards = PlayersBoards.Select(l=>l.Select(c=>c.Clone() as Card).ToList()).ToArray(),
+            };
+        }
+    }
+}
---
+++ LegendsOfCodeAndMagic/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LegendsOfCodeAndMagic
+{
+    public class Player : ICloneable
+    {
+        public List<Card> Cards { get; set; }
+        public Queue<Card> Deck { get; set; }
+
+        public int MaxMana { get; set; }
+        public int Mana { get; set; }
+        public int HP { get; set; }
+
+        public object Clone()
+        {
+            return new Player
+            {
+                Cards = this.Cards.Select(c => c.Clone() as Card).ToList(),
+                Deck = new Queue<Card>(this.Deck.Select(q => q.Clone() as Card)),
+                MaxMana = MaxMana
+            };
+        }
+    }
+}
